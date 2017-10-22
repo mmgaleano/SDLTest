48718f08bca2641b787c4352ca9635190c7d733a
mmgaleano <32870141+mmgaleano@users.noreply.github.com>
2017-10-22 14:13:52 +0800

    Screen.cpp update

--- Screen.cpp
+++ Screen.cpp
@@ -10,90 +10,89 @@
 
 namespace asiamvl {
 
-Screen::Screen():
-		m_window(NULL),m_renderer(NULL),m_texture(NULL),m_buffer(NULL){
+Screen::Screen() :
+		m_window(NULL), m_renderer(NULL), m_texture(NULL), m_buffer(NULL) {
 
 }
 
-bool Screen::init(){
+bool Screen::init() {
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		return false;
+	}
 
-	if(SDL_Init(SDL_INIT_EVERYTHING) < 0){
+	m_window = SDL_CreateWindow("Particle Fire Explosion",
+	SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+			SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
-			return false;
-		}
+	if (m_window == NULL) {
+		SDL_Quit();
+		return false;
+	}
 
-		m_window = SDL_CreateWindow("Dust Explosion",
-	                              SDL_WINDOWPOS_UNDEFINED,
-								  SDL_WINDOWPOS_UNDEFINED,
-								  SCREEN_WIDTH, SCREEN_HEIGHT,
-	                              SDL_WINDOW_SHOWN);
+	m_renderer = SDL_CreateRenderer(m_window, -1, SDL_RENDERER_PRESENTVSYNC);
+	m_texture = SDL_CreateTexture(m_renderer, SDL_PIXELFORMAT_RGBA8888,
+			SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-	    if(m_window == NULL){
-	    	SDL_Quit();
-	    	return false;
-	    }
+	if (m_renderer == NULL) {
+		SDL_DestroyWindow(m_window);
+		SDL_Quit();
+		return false;
+	}
 
-	    //For changing background to black
-	    m_renderer = SDL_CreateRenderer(m_window, -1,SDL_RENDERER_PRESENTVSYNC);
+	if (m_texture == NULL) {
+		SDL_DestroyRenderer(m_renderer);
+		SDL_DestroyWindow(m_window);
+		SDL_Quit();
+		return false;
+	}
 
-		if(m_renderer == NULL) {
-			SDL_DestroyWindow(m_window);
-			SDL_Quit();
-			return false;
-		}
+	m_buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
 
-		m_texture = SDL_CreateTexture(m_renderer, SDL_PIXELFORMAT_RGBA8888,
-				SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+	memset(m_buffer, 0, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
 
-		if(m_texture == NULL) {
-			SDL_DestroyRenderer(m_renderer);
-			SDL_DestroyWindow(m_window);
-			SDL_Quit();
-			return false;
-		}
+	return true;
+}
 
+void Screen::setPixel(int x, int y, Uint8 red, Uint8 green, Uint8 blue) {
 
-		m_buffer = new Uint32[SCREEN_WIDTH*SCREEN_HEIGHT];
+	Uint32 color = 0;
 
-		memset(m_buffer, 0, SCREEN_WIDTH*SCREEN_HEIGHT*sizeof(Uint32));
+	color += red;
+	color <<= 8;
+	color += green;
+	color <<= 8;
+	color += blue;
+	color <<= 8;
+	color += 0xFF;
 
-		//buffer[30000] = 0XFFFFFFFF;
+	m_buffer[(y * SCREEN_WIDTH) + x] = color;
+}
 
-		for(int i=0; i < SCREEN_WIDTH*SCREEN_HEIGHT; i++){
-			m_buffer[i] = 0XFF0000FF;
-		}
-
-		SDL_UpdateTexture(m_texture, NULL, m_buffer, SCREEN_WIDTH*sizeof(Uint32));
-		SDL_RenderClear(m_renderer);
-		SDL_RenderCopy(m_renderer, m_texture, NULL, NULL);
-		SDL_RenderPresent(m_renderer);
-		return true;
+void Screen::update() {
+	SDL_UpdateTexture(m_texture, NULL, m_buffer, SCREEN_WIDTH * sizeof(Uint32));
+	SDL_RenderClear(m_renderer);
+	SDL_RenderCopy(m_renderer, m_texture, NULL, NULL);
+	SDL_RenderPresent(m_renderer);
 
 }
 
-bool Screen::processEvents(){
-
+bool Screen::processEvents() {
 	SDL_Event event;
 
-	//Fetch the latest event
-	while(SDL_PollEvent(&event)){
-
-		if(event.type == SDL_QUIT){
-
+	while (SDL_PollEvent(&event)) {
+		if (event.type == SDL_QUIT) {
 			return false;
 		}
 	}
 	return true;
 }
 
-void Screen::close(){
-
-	 // Cleanup and Quit
-		delete [] m_buffer;
-		SDL_DestroyRenderer(m_renderer);
-		SDL_DestroyTexture(m_texture);
-		SDL_DestroyWindow(m_window);
-		SDL_Quit();
+void Screen::close() {
+	delete[] m_buffer;
+	SDL_DestroyRenderer(m_renderer);
+	SDL_DestroyTexture(m_texture);
+	SDL_DestroyWindow(m_window);
+	SDL_Quit();
 }
 
 } /* namespace asiamvl */

c6f3087b2d7a88de6594b155020979fa510b39e9
mmgaleano <32870141+mmgaleano@users.noreply.github.com>
2017-11-01 19:05:22 +0800

    Particle.cpp update

--- Particle.cpp
+++ Particle.cpp
@@ -6,18 +6,28 @@
  */
 
 #include <stdlib.h>
+#include <Math.h>
 #include "Particle.h"
 
 namespace asiamvl {
 
-Particle::Particle() {
+Particle::Particle(): m_x(0), m_y(0) {
 
-	m_x = ((2.0 * rand())/RAND_MAX) - 1;  //Range from -1 to 1
-	m_y = ((2.0 * rand())/RAND_MAX) - 1;  //Range from -1 to 1
+	m_direction = (2 * M_PI * rand())/RAND_MAX;
+	m_speed = (0.001 * rand())/RAND_MAX;
 }
 
 Particle::~Particle() {
 
+}
+
+void Particle::update(){
+
+	double xspeed = m_speed * cos(m_direction);
+	double yspeed = m_speed * sin(m_direction);
+
+	m_x += xspeed;
+	m_y += yspeed;
 }
 
 } /* namespace asiamvl */

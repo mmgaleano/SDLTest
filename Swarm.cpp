e12cf2028be93d97f0b44fd6da46d67442f36650
mmgaleano <32870141+mmgaleano@users.noreply.github.com>
2017-11-01 19:07:16 +0800

    Update Swarm.cpp

--- Swarm.cpp
+++ Swarm.cpp
@@ -20,4 +20,12 @@ Swarm::~Swarm() {
 	delete[] m_pParticles;
 }
 
+void Swarm::update(){
+
+	for(int i = 0; i < Swarm::NPARTICLES; i++){
+
+		m_pParticles[i].update();
+	}
+}
+
 } /* namespace asiamvl */

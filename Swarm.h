b98fca0ee01d77333b5434de3532c32d5e774800
mmgaleano <32870141+mmgaleano@users.noreply.github.com>
2017-11-03 20:21:37 +0800

    Update Swarm.h

--- Swarm.h
+++ Swarm.h
@@ -17,11 +17,12 @@ public:
 	const static int NPARTICLES = 5000;
 private:
 	Particle* m_pParticles;
+	int lastTime;
 public:
 	Swarm();
 	virtual ~Swarm();
 	const Particle* const getParticles(){return m_pParticles;}
-	void update();
+	void update(int elapsed);
 };
 
 } /* namespace asiamvl */

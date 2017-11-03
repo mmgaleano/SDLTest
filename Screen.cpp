6d370aba7b345512c8bc88b7b04bf6ebe25c9aab
mmgaleano <32870141+mmgaleano@users.noreply.github.com>
2017-11-03 20:20:33 +0800

    Update Screen.cpp

--- Screen.cpp
+++ Screen.cpp
@@ -11,7 +11,7 @@
 namespace asiamvl {
 
 Screen::Screen() :
-		m_window(NULL), m_renderer(NULL), m_texture(NULL), m_buffer(NULL) {
+		m_window(NULL), m_renderer(NULL), m_texture(NULL), m_buffer1(NULL), m_buffer2(NULL) {
 
 }
 
@@ -46,9 +46,12 @@ bool Screen::init() {
 		return false;
 	}
 
-	m_buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+	m_buffer1 = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+	m_buffer2 = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
 
-	memset(m_buffer, 0, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+	//Clearing for avoiding any garbage
+	memset(m_buffer1, 0, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+	memset(m_buffer2, 0, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
 
 	return true;
 }
@@ -71,11 +74,11 @@ void Screen::setPixel(int x, int y, Uint8 red, Uint8 green, Uint8 blue) {
 	color <<= 8;
 	color += 0xFF;
 
-	m_buffer[(y * SCREEN_WIDTH) + x] = color;
+	m_buffer1[(y * SCREEN_WIDTH) + x] = color;
 }
 
 void Screen::update() {
-	SDL_UpdateTexture(m_texture, NULL, m_buffer, SCREEN_WIDTH * sizeof(Uint32));
+	SDL_UpdateTexture(m_texture, NULL, m_buffer1, SCREEN_WIDTH * sizeof(Uint32));
 	SDL_RenderClear(m_renderer);
 	SDL_RenderCopy(m_renderer, m_texture, NULL, NULL);
 	SDL_RenderPresent(m_renderer);
@@ -94,16 +97,67 @@ bool Screen::processEvents() {
 }
 
 void Screen::close() {
-	delete[] m_buffer;
+	delete[] m_buffer1;
+	delete[] m_buffer2;
 	SDL_DestroyRenderer(m_renderer);
 	SDL_DestroyTexture(m_texture);
 	SDL_DestroyWindow(m_window);
 	SDL_Quit();
 }
 
-void Screen::clear(){
 
-	memset(m_buffer, 0, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+void Screen::boxBlur(){
+
+	//Swap the buffers, so pixels are in m_buffer2 and we are drawing to m_buffer1
+
+	Uint32 *temp = m_buffer1;
+
+	m_buffer1 = m_buffer2;
+	m_buffer2 = temp;
+
+	for(int y=0; y<SCREEN_HEIGHT; y++){
+
+		for(int x=0; x<SCREEN_WIDTH; x++){
+
+
+			/*
+			 * 0 0 0
+			 * 0 1 0
+			 * 0 0 0
+			 */
+
+			int redTotal = 0;
+			int greenTotal = 0;
+			int blueTotal = 0;
+
+			for(int row=-1; row<=1; row++){
+				for(int col=-1; col<=1; col++){
+
+					int currentX = x + col;
+					int currentY = y + row;
+
+					if(currentX >=0 && currentX < SCREEN_WIDTH && currentY >=0 && currentY < SCREEN_HEIGHT){
+
+						Uint32 color = m_buffer2[currentY * SCREEN_WIDTH + currentX];
+
+						Uint8 red = color >> 24;
+						Uint8 green = color >> 16;
+						Uint8 blue = color >> 8;
+
+						redTotal += red;
+						greenTotal += green;
+						blueTotal += blue;
+					}
+				}
+			}
+
+			Uint8 red = redTotal/9;
+			Uint8 green = greenTotal/9;
+			Uint8 blue = blueTotal/9;
+
+			setPixel(x, y, red, green, blue);
+		}
+	}
 }
 
 } /* namespace asiamvl */
